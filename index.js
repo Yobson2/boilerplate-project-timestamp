174739eb3aff4189df897418662988687bdb8e50
yoboue louis <yobouelouiscarlos@gmail.com>
2023-05-15 03:14:54 +0200

    projet 1 terminé

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // index.js
 // where your node app starts
-
+require('dotenv').config()
 // init project
 var express = require('express');
 var app = express();
+const port=3000
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,9 +25,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res, next) => {
+  const dateParam = req.params.date;
+  let timestamp;
+  let time;
+
+  if (Number(dateParam)) {
+    timestamp = parseInt(dateParam);
+    time = new Date(timestamp).toString().slice(0, 28);
+  } else {
+    time = new Date(dateParam).toString().slice(0, 28);
+    timestamp = new Date(dateParam).getTime();
+  }
+
+  res.json({
+    "unix": timestamp,
+    "utc": time
+  });
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+app.listen(port,()=> console.log('Your app is listening on port ' + port))
